76b41edceb435f2f797cc1a8fd8336e0b09299ba
Alejandro Gonzalez <alexgve7sv@outlook.com>
2023-08-23 09:51:25 +0200

    uploading gif app

--- giftexpertapp/src/App.jsx
+++ giftexpertapp/src/App.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react"
+import AddGift from "./components/addGif"
+import GifGrid from "./components/GifGrid"
+
 export const App = () => {
+    const [categories, setCategories] = useState(['One punch'])
+
+    const onAddCategory = (value) => {
+        if (categories.includes(value)) {
+            return
+        }
+        setCategories([...categories, value])
+    }
+
     return (
         <>
+            <AddGift onAddCategory={onAddCategory} />
+            <ol>
+                {
+                    categories.map((category, index) => {
+                        return <GifGrid
+                            key={index}
+                            category={category}
+                        />
+                    })
+                }
+            </ol>
+
         </>
     )
 }
